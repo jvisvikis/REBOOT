9723a1baf03632e611027ac56ddfd95023fb2878
jvisvikis <69331363+jvisvikis@users.noreply.github.com>
2022-09-20 16:51:50 +1000

    save changes oidk im tired

--- Assets/Scripts/Reboot.cs
+++ Assets/Scripts/Reboot.cs
@@ -6,57 +6,64 @@ using TMPro;
 
 public class Reboot : Interactable 
 {
-    // public GameObject screenPanel;
-    // public Text passcodeTXT;
-    // public TMP_InputField iField;
-    // public Walk player;
-    // private ComputerState computer;
-    // private string passcode;
-    // private bool rebooting;
+    private GameObject screenPanel;
+    private Text passcodeTXT;
+    private TMP_InputField iField;
+    private Walk player;
+    private ComputerState computer;
+    private string passcode;
+    private bool rebooting;
 
-    // // Start is called before the first frame update
-    // void Start()
-    // {
-    //     computer = GetComponent<ComputerState>();
-    //     passcode = "11111111";
-    //     passcodeTXT.text = passcode;
-    // }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
 
-    // // Update is called once per frame
-    // void Update()
-    // {
-    //     if(rebooting && Input.GetKeyDown(KeyCode.Return))
-    //     {
-    //         EndRebootSequence();
-    //     }
-    // }
+    // Update is called once per frame
+    void Update()
+    {
+        if(rebooting && Input.GetKeyDown(KeyCode.Return))
+        {
+            EndRebootSequence();
+        }
+    }
 
-    // public void StartRebootSequence()
-    // {
-    //     player.SetRebooting(true);
-    //     Camera.main.GetComponent<FPC>().enabled = false;
-    //     rebooting = true;
-    //     screenPanel.SetActive(true);
-    //     Cursor.lockState = CursorLockMode.None;
-    //     Cursor.visible = true;
-    // }
+    public void StartRebootSequence()
+    {
+        screenPanel = GameObject.Find("InputPasscodePanel");
+        computer = GetComponent<ComputerState>();
+        Debug.Log(screenPanel);
+        passcodeTXT = notepad.np.gameObject.GetComponent<Text>();
+        iField = GameObject.Find("InputField (TMP)").GetComponent<TMP_InputField>();
+        player = FindObjectOfType<Walk>().GetComponent<Walk>();
+        passcode = "11111111";
+        passcodeTXT.text = passcode;
+        screenPanel.SetActive(false);
+        player.SetRebooting(true);
+        Camera.main.GetComponent<FPC>().enabled = false;
+        rebooting = true;
+        screenPanel.SetActive(true);
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+    }
 
-    // public void EndRebootSequence()
-    // {
-    //     if(!iField.text.Equals(passcode))
-    //     {
-    //         return;
-    //     }
-    //     Camera.main.GetComponent<FPC>().enabled = true;
-    //     player.SetRebooting(false);
-    //     rebooting = false;
-    //     screenPanel.SetActive(false);
-    //     Cursor.lockState = CursorLockMode.Locked;
-    //     Cursor.visible = false;
-    // }
+    public void EndRebootSequence()
+    {
+        if(!iField.text.Equals(passcode))
+        {
+            return;
+        }
+        Camera.main.GetComponent<FPC>().enabled = true;
+        player.SetRebooting(false);
+        rebooting = false;
+        screenPanel.SetActive(false);
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
 
     public override void behaviour(){
-     //   StartRebootSequence();
+        StartRebootSequence();
     }
 
 }
---
+++ Assets/notepad.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class notepad : MonoBehaviour
+{
+    public static notepad np;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+       np = this; 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
